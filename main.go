7322d9c9e0491b875f24cddd98186e718725a2c4
Diego Almeida <dyhalmeida@gmail.com>
2023-07-18 09:29:48 -0300

    feat: add open database connection and execute usecase

--- main.go
+++ main.go
@@ -1,5 +1,32 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/dyhalmeida/golang-order/internal/infra/database"
+	"github.com/dyhalmeida/golang-order/internal/usecase"
+)
+
 func main() {
-	println("Hello World")
+	db, err := sql.Open("sqlite3", "db.sqlite3")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	input := usecase.OrderInput{
+		ID:    "0001",
+		Price: 10.0,
+		Tax:   1.0,
+	}
+
+	output, err := uc.Execute(input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(output)
 }
