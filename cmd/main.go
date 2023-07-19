a9a7e677a5fc84b0e249812b662e9ad218917c40
Diego Almeida <dyhalmeida@gmail.com>
2023-07-18 23:32:01 -0300

    feat: add rabbitmq queue

---
+++ cmd/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+
+	_ "github.com/lib/pq"
+
+	"github.com/dyhalmeida/golang-order/internal/infra/database"
+	"github.com/dyhalmeida/golang-order/internal/usecase"
+	"github.com/dyhalmeida/golang-order/pkg/rabbitmq"
+)
+
+func main() {
+	db, err := sql.Open("postgres", "postgresql://postgres:postgres@localhost:5432/orders?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	orderRepository := database.NewOrderRepository(db)
+	uc := usecase.NewCalculateFinalPrice(orderRepository)
+
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, uc)
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc *usecase.CalculateFinalPrice) {
+	fmt.Println("Starting rabbitmq")
+	for msg := range msgChan {
+		var input usecase.OrderInput
+		err := json.Unmarshal(msg.Body, &input)
+		if err != nil {
+			panic(err)
+		}
+		output, err := uc.Execute(input)
+		if err != nil {
+			panic(err)
+		}
+		msg.Ack(false)
+		fmt.Println("Mensagem processada e salva no banco:", output)
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/google/uuid v1.3.0
 	github.com/lib/pq v1.10.9
+	github.com/rabbitmq/amqp091-go v1.8.1
 	github.com/stretchr/testify v1.8.4
 )
 
